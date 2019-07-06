2a5167ca115ee875a28dfe05c1a4cbaa2da9d51a
Alex de Moraes <alexdemoraes@gmail.com>
2019-07-06 17:38:01 -0300

    Adding Transfer Entity and DAO structure

---
+++ src/main/java/com/rf/challenge/dao/TransferDAOImpl.java
@@ -0,0 +1,20 @@
+package com.rf.challenge.dao;
+
+import com.rf.challenge.dao.model.TransferEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class TransferDAOImpl implements TransferDAO {
+
+    @Override
+    public void create(TransferEntity entity) {
+
+    }
+
+    @Override
+    public List<TransferEntity> list() {
+        return null;
+    }
+}
---
+++ src/main/java/com/rf/challenge/dao/model/TransferEntity.java
@@ -0,0 +1,93 @@
+package com.rf.challenge.dao.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class TransferEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String originAccountNumber;
+    private String destinationAccountNumber;
+    private BigDecimal amount;
+    private BigDecimal fee;
+    private LocalDate transferDate;
+    private LocalDate scheduleDate;
+
+    public TransferEntity() {
+    }
+
+    public TransferEntity(String originAccountNumber, String destinationAccountNumber,
+                          BigDecimal amount, BigDecimal fee, LocalDate transferDate,
+                          LocalDate scheduleDate) {
+        this.originAccountNumber = originAccountNumber;
+        this.destinationAccountNumber = destinationAccountNumber;
+        this.amount = amount;
+        this.fee = fee;
+        this.transferDate = transferDate;
+        this.scheduleDate = scheduleDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOriginAccountNumber() {
+        return originAccountNumber;
+    }
+
+    public void setOriginAccountNumber(String originAccountNumber) {
+        this.originAccountNumber = originAccountNumber;
+    }
+
+    public String getDestinationAccountNumber() {
+        return destinationAccountNumber;
+    }
+
+    public void setDestinationAccountNumber(String destinationAccountNumber) {
+        this.destinationAccountNumber = destinationAccountNumber;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getFee() {
+        return fee;
+    }
+
+    public void setFee(BigDecimal fee) {
+        this.fee = fee;
+    }
+
+    public LocalDate getTransferDate() {
+        return transferDate;
+    }
+
+    public void setTransferDate(LocalDate transferDate) {
+        this.transferDate = transferDate;
+    }
+
+    public LocalDate getScheduleDate() {
+        return scheduleDate;
+    }
+
+    public void setScheduleDate(LocalDate scheduleDate) {
+        this.scheduleDate = scheduleDate;
+    }
+}
