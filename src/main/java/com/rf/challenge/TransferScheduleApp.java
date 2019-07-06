b4d15306abe3b29f7991e577779e5d018086ab66
Alex de Moraes <alexdemoraes@gmail.com>
2019-07-06 08:45:06 -0300

    Main App

---
+++ src/main/java/com/rf/challenge/TransferScheduleApp.java
@@ -0,0 +1,14 @@
+package com.rf.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferScheduleApp {
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(TransferScheduleApp.class, args);
+    }
+
+}
